c45ea14bb2a80bf321d4dd71f761ca5ae5d0c809
Abdul Wahab <cs.abdulwahab@gmail.com>
2018-11-30 16:03:17 +0500

    FirstCommit

---
+++ app/src/main/java/com/example/abdul_wahab/bnuimageupload/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.abdul_wahab.bnuimageupload;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_second);
+        String s = "";
+
+
+    }
+}
